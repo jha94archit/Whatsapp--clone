4f7ad882809a18f83e285fe5b0c936c07cf10cba
aj068102 <aj068102@cerner.net>
2021-05-31 12:27:01 +0530

    Set up of react app and basic cleaning of project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCJaPjR0qxKHrTe-Hdn8t347m7EGxMtYE4",
+    authDomain: "whatsapp-clone-c072b.firebaseapp.com",
+    projectId: "whatsapp-clone-c072b",
+    storageBucket: "whatsapp-clone-c072b.appspot.com",
+    messagingSenderId: "681943422760",
+    appId: "1:681943422760:web:21f08f3e14303f0bbf221d",
+    measurementId: "G-6GWEPL6XMY"
+  };
